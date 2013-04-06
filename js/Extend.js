b865a8dfec4104dbdf2de45150ddb55c649b4ab3
Vigneswaran <vignes.marimuthu@gmail.com>
2013-04-06 20:52:25 +0530

    Added new way extending superclass

--- js/Extend.js
+++ js/Extend.js
@@ -3,36 +3,29 @@
  * 
  * @author Vigneswaran Marimuthu
  * 
- * @param supercls
  * @param config
  * 
  * @returns Function
  */
-function extend(supercls, config){
-  if(!isFunction(supercls)){
-		console.warn("Passed superclass is not a function.");
-		return;
-	}
-	var initialize = true;
+
+var initialize = true;
+
+Function.prototype.extend = function(config){
 	
 	initialize = false;
-	var proto = new supercls();
-	subclass.superclass = new supercls();
+	var proto = new this();
 	initialize = true;
-	
 	for(var prop in config)
 		proto[prop] = config[prop];
 	
-	function subclass(){
+	var subclass = function(){
 		if(initialize && this.init) this.init.call(this, this);
-	}
+	};
 	
 	subclass.prototype = proto;
-	subclass.constructor = supercls;
+	subclass.superclass = this.prototype;
+	subclass.constructor = this;
 	
 	return subclass;
-}
-
-function isFunction(value){
-	return Object.prototype.toString.call(value) === "[object Function]";
-}
+	
+};
